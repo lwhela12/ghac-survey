756229328cf3030d94483396dc75e125a254bf04
lwhela12 <lucas@whelanpartners.com>
2025-08-14 12:28:50 -0700

    fix: Remove unused useEffect import in Ranking component

--- frontend/src/components/Survey/QuestionTypes/Ranking.tsx
+++ frontend/src/components/Survey/QuestionTypes/Ranking.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Question } from '../../../types/survey';
 
